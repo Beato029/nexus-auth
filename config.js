04181b2fa6d38c308370e3aefacfbfb08a4331b4
Beato029 <kalibeato1983@gmail.com>
2026-04-27 00:00:43 +0200

    commit

--- config.js
+++ config.js
@@ -8,5 +8,5 @@
 
 const NEXUS_CONFIG = {
   supabaseUrl:     'https://wvlwijfsitkybipkkmdd.supabase.co',       // es. https://xyzabcdef.supabase.co
-  supabaseAnonKey: 'sb_publishable_1jbf5VZet8fBQ0cJq2_2OQ_5LGqU09u',  // la chiave "anon public" (sicura da esporre)
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bHdpamZzaXRreWJpcGtrbWRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMzAxOTMsImV4cCI6MjA5MjgwNjE5M30.0ZitGQFu2TMp7b1Egk5ncGpLcTx5j_c-nwOLrqTSRUU',  // la chiave "anon public" (sicura da esporre)
 };
